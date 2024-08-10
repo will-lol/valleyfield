c39d9a0d6e4cf95415674c84710ba38afb695b84
will <will.bradshaw50@gmail.com>
2024-08-10 23:07:24 +1000

    update vendor hash

--- infra/default.nix
+++ infra/default.nix
@@ -2,5 +2,5 @@
 	name = "cdkapp";
 	src = ./.;
 	CGO_ENABLED = 0;
-	vendorHash = "sha256-wFJJaKopcICdIW51Lhno5z0nhI930YNtIxkC2akGyvg=";
+	vendorHash = "sha256-NDxcoQABmxrfMEAzavgiRIEdq6dP+dXba4ITzWPyg6g=";
 }
